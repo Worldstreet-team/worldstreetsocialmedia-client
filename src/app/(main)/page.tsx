7ba842292630140e121d27753241f801295a2f0f
nelson-david <larva.code15@gmail.com>
2026-02-05 18:16:02 +0100

    Initial Commit

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Home Feed</h1>
+        </div>
+    )
+}
---
+++ src/app/stories/page.tsx
@@ -0,0 +1,7 @@
+export default function StoriesPage() {
+    return (
+        <div>
+            <h1>Stories</h1>
+        </div>
+    )
+}
