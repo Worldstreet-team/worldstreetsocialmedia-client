44cbf7cac1f1c66eedd0701b8b75bfc4457e034d
nelson-david <larva.code15@gmail.com>
2026-02-11 16:18:13 +0100

    Added 222333

--- src/components/layout/LeftSidebar.tsx
+++ src/components/layout/LeftSidebar.tsx
@@ -14,7 +14,7 @@ export function LeftSidebar() {
 	return (
 		<header className="w-[275px] hidden md:flex flex-col sticky top-0 h-screen px-2 overflow-y-auto">
 			<div className="py-1 px-3 mb-1">
-				<div className="w-12 h-12 flex items-center justify-center hover:bg-blue-50 rounded-full cursor-pointer transition-colors">
+				<div className="flex items-center justify-center hover:bg-blue-50 rounded-full cursor-pointer transition-colors">
 					<h1 className="text-xl">WorldStreet</h1>
 				</div>
 			</div>
